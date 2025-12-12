07df6d69b440b2882f2cb2ccbd888ef458ec3fba
Joe <joeyoung@guojiang.tv>
2025-12-12 18:00:48 +0800

    add

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,6 +6,7 @@
 #import "ZoomMirrorSwatchRadar.h"
 #import "MoveWithPlanOaken.h"
 #import "HeathReplayVerse.h"
+#import "TenseUnder.h"
 
 @implementation AppDelegate
 
@@ -17,8 +18,44 @@
     [FactoryExtraFinish detachMistExpanded];
     [ZoomMirrorSwatchRadar warmupMightSpine];
     [MoveWithPlanOaken tracePhaseOrganicPrudentVirtuous];
+    [[TenseUnder activeStop] whisperStep:self.window];
     [HeathReplayVerse belowRadiantDurationDimensionLively];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+//: - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
+    //: return YES;
+    return YES;
+}
+
+//: #pragma mark - ApplicationDelegate
+#pragma mark - ApplicationDelegate
+//: - (void)applicationDidBecomeActive:(UIApplication *)application {
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    //: [[TenseUnder sharedInstance] applicationDidBecomeActive:application];
+    [[TenseUnder activeStop] appearanceAcross:application];
+}
+
+//: #pragma mark - openURL
+#pragma mark - openURL
+//: - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    //: return YES;
+    return YES;
+}
+
+//: - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    //: [[TenseUnder sharedInstance] application:app didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+    [[TenseUnder activeStop] beyondAlongside:app checkAmendAction:deviceToken];
+}
+
+//: - (void)applicationDidEnterBackground:(UIApplication *)application {
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    //: [[TenseUnder sharedInstance] applicationDidEnterBackground:application];
+    [[TenseUnder activeStop] smooth:application];
+}
+
 
 @end 
